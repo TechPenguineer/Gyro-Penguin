a9de7fa73320a1d3cd38e62bbc80dad44ad31465
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-30 22:04:04 -0400

    create window

--- src/loader.py
+++ src/loader.py
@@ -1,1 +1,14 @@
-from PyQt5 
+from PyQt5 import *
+from PyQt5.QtWidgets import *
+import sys
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200,200,300,300)
+    win.setWindowTitle("Gyro Penguin")
+    
+    win.show()
+    sys.exit(app.exec_())
+
+window()
