6b47340ffec89bc94263f2c0d2fd0ee83fca3829
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-30 22:16:55 -0400

    create 3d grid

--- src/loader.py
+++ src/loader.py
@@ -1,14 +1,56 @@
-from PyQt5 import *
-from PyQt5.QtWidgets import *
+
+
+from pyqtgraph.Qt import QtCore, QtGui
+import pyqtgraph.opengl as gl
+import pyqtgraph as pg
+import numpy as np
 import sys
 
-def window():
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200,200,300,300)
-    win.setWindowTitle("Gyro Penguin")
-    
-    win.show()
-    sys.exit(app.exec_())
 
-window()
+class Visualizer(object):
+    def __init__(self):
+        self.traces = dict()
+        self.app = QtGui.QApplication(sys.argv)
+        self.w = gl.GLViewWidget()
+        self.w.opts['distance'] = 40
+        self.w.setWindowTitle('Gyro Penguin')
+        self.w.setGeometry(0, 110, 1920, 1080)
+        self.w.show()
+
+        # create the background grids
+ 
+        gz = gl.GLGridItem()
+        gz.translate(0, 0, -10)
+        self.w.addItem(gz)
+
+        self.n = 50
+        self.m = 1000
+        self.y = np.linspace(-10, 10, self.n)
+        self.x = np.linspace(-10, 10, self.m)
+        self.phase = 0
+
+        for i in range(self.n):
+            ...
+
+    def start(self):
+        if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+            QtGui.QApplication.instance().exec_()
+
+    def set_plotdata(self, name, points, color, width):
+        self.traces[name].setData(pos=points, color=color, width=width)
+
+    def update(self):
+        ...
+
+
+    def animation(self):
+        timer = QtCore.QTimer()
+        timer.timeout.connect(self.update)
+        timer.start(10)
+        self.start()
+
+
+# Start Qt event loop unless running in interactive mode.
+if __name__ == '__main__':
+    v = Visualizer()
+    v.animation()
